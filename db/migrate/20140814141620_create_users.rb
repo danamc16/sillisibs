5cd4710f862ed3a6d85c174c20417fdb68a0da04
Dana Chaykovsky <dana.chaykovsky@yale.edu>
2014-08-14 10:42:56 -0400

    Created user and family models

---
+++ db/migrate/20140814141620_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :presence
+      t.integer :family_id
+
+      t.timestamps
+    end
+    add_index :users, :family_id
+  end
+end
